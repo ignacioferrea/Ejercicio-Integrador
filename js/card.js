b2bf25976c300a61f1142d05a9b13e50943b0cd3
Ignacio Ferrea <ferreaignacio@gmail.com>
2022-11-07 20:20:48 -0300

    Termino método appendTo

--- js/card.js
+++ js/card.js
@@ -1,9 +1,11 @@
 function Card(heading, description, image) {
+    // Propiedades/Atributos
     this.heading = heading,
     this.description = description,
     this.image = image
 
-    this.appendTo = function () {
+    // Método
+    this.appendTo = function (destinationElement) {
         let card = document.createElement('a')
         card.classList.add('card')
         card.href = 'https://sony.com'
@@ -14,18 +16,19 @@ function Card(heading, description, image) {
                 <div class="card__image-container">
                     <img
                         class="card__image"
-                        src="${image}"
-                        alt="${heading}"
+                        src="${this.image}"
+                        alt="${this.heading}"
                     />
                 </div>
                 <div class="card__content">
-                    <h2 class="card__heading">${heading}</h2>
+                    <h2 class="card__heading">${this.heading}</h2>
                     <div class="card__description">
-                        <p>${description}</p>
+                        <p>${this.description}</p>
                     </div>
                 </div>
             </article>
-        ` 
+        `
+        destinationElement.appendChild(card) 
     }
 }
 
@@ -35,16 +38,88 @@ console.log(elemCardsContainer)
 
 const card1 = new Card(
     'Celular', 
-    'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    'apple-iphone-11-pro-max',
     'img/productos/apple-iphone-11-pro-max.jpg'
 )
 
 const card2 = new Card(
     'Drone', 
-    'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    'dji-mavic-2-pro',
     'img/productos/dji-mavic-2-pro.jpg'
+)
+
+const card3 = new Card(
+    'Celular', 
+    'apple-iphone-12',
+    'img/productos/apple-iphone-12.jpg'
+)
+
+const card4 = new Card(
+    'Auriculares', 
+    'auriculares-sony',
+    'img/productos/auriculares-sony.jpg'
+)
+
+const card5 = new Card(
+    'Camara', 
+    'camara-canon',
+    'img/productos/camara-canon.jpg'
+)
+
+const card6 = new Card(
+    'Televisor', 
+    'televisor-lg',
+    'img/productos/televisor-lg.jpg'
+)
+
+const card7 = new Card(
+    'Consola', 
+    'sony-playstation-5',
+    'img/productos/sony-playstation-5.jpg'
+)
+
+const card8 = new Card(
+    'Celular', 
+    'samsung-galaxy-s21-plus-5g',
+    'img/productos/samsung-galaxy-s21-plus-5g.jpg'
+)
+
+const card9 = new Card(
+    'Modem', 
+    'modem-motorola',
+    'img/productos/modem-motorola.jpg'
 )
 
 console.log(card1)
 console.log(card2)
+console.log(card3)
+console.log(card4)
+console.log(card5)
+console.log(card6)
+console.log(card7)
+console.log(card8)
+console.log(card9)
 
+const cards = [
+    card1,
+    card2,
+    card3,
+    card4,
+    card5,
+    card6,
+    card7,
+    card8,
+    card9
+]
+
+console.log(cards)
+
+card1.appendTo(elemCardsContainer)
+card2.appendTo(elemCardsContainer)
+card3.appendTo(elemCardsContainer)
+card4.appendTo(elemCardsContainer)
+card5.appendTo(elemCardsContainer)
+card6.appendTo(elemCardsContainer)
+card7.appendTo(elemCardsContainer)
+card8.appendTo(elemCardsContainer)
+card9.appendTo(elemCardsContainer)
