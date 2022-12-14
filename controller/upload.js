f9c19f10c31f60526668f654c7a987f9261ddbb5
Ignacio Ferrea <ferreaignacio@gmail.com>
2022-12-14 16:35:58 -0300

    Listo uploadImage

---
+++ controller/upload.js
@@ -0,0 +1,15 @@
+const uploadImagen = (req, res) => {
+    const file = req.file
+    
+    if (!file) {
+        const error = new Error('Error subiendo el archivo')
+        error.httpStatuscode = 400
+        return next(error)
+    }
+    
+    res.json({ nombre: file.filename })
+}
+
+module.exports = {
+    uploadImagen
+}
--- routers/upload.js
+++ routers/upload.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const routerUpload = express.Router()
 
+const upload = require('../config/multer')
+const controller = require('../controller/upload')
+
 /* POST - Request para agregar producto */
-routerUpload.post('/', (req, res) => {
-    res.send('Recibiendo imagen')
-})
+routerUpload.post('/', upload.single('foto'), controller.uploadImagen)
 
 module.exports = routerUpload
