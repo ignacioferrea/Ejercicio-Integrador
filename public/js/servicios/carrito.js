28b6ab3688545a7d889ce4ed7e8a0752f5fac6ac
Ignacio Ferrea <ferreaignacio@gmail.com>
2022-12-15 15:13:55 -0300

    Cambio endpoint en carrito servicio y arreglo id en la plantilla de carrito

--- public/js/servicios/carrito.js
+++ public/js/servicios/carrito.js
@@ -1,5 +1,5 @@
 class CarritoService {
-    URL_CARRITO = 'https://6372b00a348e947299fb5255.mockapi.io/carrito/'
+    URL_CARRITO = '/api/carrito/'
 
     async guardarCarritoServicio(carrito) {
         const carritoGuardado = await http.post(this.URL_CARRITO, carrito)
